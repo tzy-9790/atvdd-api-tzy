6640a5c2be5359f61ca5f87c37bb88fc1a25d4c5
Litzy-Marisol <litzmarisol@gmail.com>
2025-11-01 16:09:30 -0300

    Initial commit: project structure from Spring Initializr

---
+++ atvdd/src/test/java/com/atvddtzy/atvdd/AtvddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atvddtzy.atvdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtvddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
